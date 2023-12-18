042781439eb6588b6e8275bc318b1892675fc0bd
ADE DAYO <adedayoemmanuel729@gmail.com>
2023-12-18 11:24:25 +0100

    hidden text snippet complete

--- snippet.js
+++ snippet.js
@@ -1,64 +1,65 @@
 let htmlDiv = document.querySelector(".html");
 htmlDiv.innerText = `        <div class="container">
-<div class="sub">
-    <img src="images/html.svg" alt="">
-    <div class="text"><p>html</p></div>
-</div>
-<div class="sub">
-    <img src="images/css.svg" alt="">
-    <div class="text"><p>css</p></div>
-</div>
-<div class="sub">
-    <img src="images/php.svg" alt="">
-    <div class="text"><p>php</p></div>
-</div>
-<div class="sub">
-    <img src="images/javascript.svg" alt="">
-    <div class="text"><p>javascript</p></div>
-</div>
+\b<div class="sub">
+\b\b<img src="images/html.svg" alt="">
+\b\b\b<div class="text"><p>html</p></div>
+\b</div>
+\b<div class="sub">
+\b\b<img src="images/css.svg" alt="">
+\b\b\b<div class="text"><p>css</p></div>
+\b</div>
+\b<div class="sub">
+\b\b<img src="images/php.svg" alt="">
+\b\b\b<div class="text"><p>php</p></div>
+\b</div>
+\b<div class="sub">
+\b\b<img src="images/javascript.svg" alt="">
+\b\b\b<div class="text">\n\b\b\b\b<p>javascript</p>\n\b\b\b</div>
+\b</div>
 </div>`
 
-document.querySelector(".scss").innerText = `    .container{
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-wrap: wrap;
-    gap: 5rem;
-    .sub{
-        &:hover{
-            .text{
-                opacity: 1;
-            }
-        }
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        width: 8rem;
-        padding: 2rem;
-        background-color: gray;
-        z-index: 3;
-        clip-path: polygon(51% 0, 100% 26%, 100% 76%, 57% 100%, 0 76%, 0 26%);
-        outline: 1px solid gray;
-        img{
-            width: 9rem;
-            height: 9rem;
-        }
-        .text{
-            position: absolute;
-            // width: 100%;
-            height: 100%;
-            opacity: 0;
-            display: flex;
-            align-items: center;
-            justify-content: center;   
-            background-color:  rgba(22, 166, 180, 0.9);
-            transition: all .3s ease-in-out;
-            p{
-                font-size: 2rem;
-                text-transform: uppercase;
-                font-weight: 1;
-                color: white;
-            }
-        }
-    }
+document.querySelector(".scss").innerText = `
+.container{
+    \b\bdisplay: flex;
+    \b\balign-items: center;
+    \b\bjustify-content: center;
+    \b\bflex-wrap: wrap;
+    \b\bgap: 5rem;
+    \b.sub{
+        \b\b\b&:hover{
+            \b\b\b\b .text{
+                \b\b\b\b\bopacity: 1;
+                \b\b\b\b}
+            \b\b\b}
+        \b\bdisplay: flex;
+        \b\balign-items: center;
+        \b\bjustify-content: center;
+        \b\bwidth: 8rem;
+        \b\bpadding: 2rem;
+        \b\bbackground-color: gray;
+        \b\bz-index: 3;
+        \b\bclip-path: polygon(51% 0, 100% 26%, 100% 76%, 57% 100%, 0 76%, 0 26%);
+        \b\boutline: 1px solid gray;
+        \b\b\bimg{
+            \b\b\b\bwidth: 9rem;
+            \b\b\b\bheight: 9rem;
+        \b\b\b}
+        \b\b\b.text{
+            \b\b\b\bposition: absolute;
+            \b\b\b\bwidth: 100%;
+            \b\b\b\bheight: 100%;
+            \b\b\b\bopacity: 0;
+            \b\b\b\bdisplay: flex;
+            \b\b\b\balign-items: center;
+            \b\b\b\bjustify-content: center;   
+            \b\b\b\bbackground-color:  rgba(22, 166, 180, 0.9);
+            \b\b\b\btransition: all .3s ease-in-out;
+            \b\b\b\bp{
+                \b\b\b\b\bfont-size: 2rem;
+                \b\b\b\b\btext-transform: uppercase;
+                \b\b\b\b\bfont-weight: 1;
+                \b\b\b\b\bcolor: white;
+            \b\b\b\b}
+        \b\b\b }
+        \b\b}
 }`;
